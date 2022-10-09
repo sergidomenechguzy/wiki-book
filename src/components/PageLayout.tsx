0d68ba8a0f619071fd52aa074dd8021bb7fd85be
Sergi Domenech Guzy <sergi.domenech_guzy@stud.htwk-leipzig.de>
2022-10-10 01:38:34 +0200

    chore: add footer

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -7,13 +7,20 @@ interface IPageLayoutProps {
 
 const PageLayout = ({ children }: IPageLayoutProps) => {
   return (
-    <div>
+    <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Wiki for a personal book project" />
       </Head>
       <main className="px-2 sm:px-4 lg:p-8">{children}</main>
-    </div>
+      <div className="flex-grow" />
+      {/* <div className="divider mb-0" /> */}
+      <footer className="footer footer-center p-4 text-base-content bg-base-200">
+        <div>
+          <p>Wiki</p>
+        </div>
+      </footer>
+    </>
   );
 };
 
