cfbcfb67e02ce5950c64a9090ba871367a99e52f
Sergi Domenech Guzy <sergi.domenech_guzy@stud.htwk-leipzig.de>
2022-10-11 17:44:13 +0200

    fix: add main bottom padding for all sizes

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -12,7 +12,7 @@ const PageLayout = ({ children }: IPageLayoutProps) => {
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Wiki for a personal book project" />
       </Head>
-      <main className="px-2 sm:px-4 lg:p-8">{children}</main>
+      <main className="px-2 pb-8 sm:px-4 lg:p-8">{children}</main>
       <div className="flex-grow" />
       {/* <div className="divider mb-0" /> */}
       <footer className="footer footer-center p-4 text-base-content bg-base-200">
