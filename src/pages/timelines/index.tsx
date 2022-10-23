e20d5b615e7378201acf62ada91e213e87e93e8d
Sergi Domenech Guzy <sergi.domenech_guzy@stud.htwk-leipzig.de>
2022-10-23 16:35:46 +0200

    feat: add timeline overview page

--- src/pages/timelines/index.tsx
+++ src/pages/timelines/index.tsx
@@ -1,26 +1,43 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
+import DensityToggle from "../../components/DensitySwap";
 import PageLayout from "../../components/PageLayout";
+import PageList from "../../components/PageList";
+import Search from "../../components/Search";
+import { getAllTimelinePageData } from "../../lib/timelinePages";
 import { buildTitle } from "../../lib/utils";
 
-const TimelinesPage: NextPage = () => {
+interface IPageOverviewPageProps {
+  timelinePageLists: LetterLists<TimelinePage>;
+}
+
+const TimelinesPage: NextPage<IPageOverviewPageProps> = ({
+  timelinePageLists,
+}) => {
+  const [denseUi, setDenseUi] = useState(false);
+
   return (
     <PageLayout>
       <Head>
         <title>{buildTitle("All Timelines")}</title>
       </Head>
-      <h1 className="text-5xl">All Timelines</h1>
+      {/* <h1 className="text-5xl">All Timelines</h1> */}
+
+      <Search />
+      <DensityToggle value={denseUi} onChange={setDenseUi} />
+      <PageList pageLists={timelinePageLists} denseUi={denseUi} />
     </PageLayout>
   );
 };
 
-// export const getStaticProps: GetStaticProps = () => {
-//   const wikiPageLists = getAllWikiPageData();
-//   return {
-//     props: {
-//       wikiPageLists,
-//     },
-//   };
-// };
+export const getStaticProps: GetStaticProps = () => {
+  const timelinePageLists = getAllTimelinePageData();
+  return {
+    props: {
+      timelinePageLists,
+    },
+  };
+};
 
 export default TimelinesPage;
